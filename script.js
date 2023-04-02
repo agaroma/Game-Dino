9298bc3869241cd01996e913348c6d8a0d87cf5b
agaroma <90196074+agaroma@users.noreply.github.com>
2023-04-03 01:10:31 +0300

    Wrote the jump animation in script.js and adjusted the seconds in css

---
+++ script.js
@@ -0,0 +1,10 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener("keydown", function(event){
+    jump();
+});
+
+function jump () {
+    dino.classList.add("jump")
+}
